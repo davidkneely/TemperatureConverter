2b8967a3fdbc6d7fc996e4506773cd5593c50588
David N <deeniri@gmail.com>
2015-02-02 14:16:31 -1000

    Second Commit adds check for second argument.

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -5,27 +5,26 @@ public class TemperatureConverter {
   public static void main(String[] args) {
     double celsius, fahrenheit = 0;
 
-    if(args[0].equals("-F")){ // initial value is in farenheit
-      fahrenheit = Double.parseDouble(args[1]);
-      celsius = (fahrenheit - 32) * 5/9;
-      System.out.println("celsius is: " + celsius);
+    if(args[1] != null){
+
+      if(args[0].equals("-F")){ // initial value is in farenheit
+        fahrenheit = Double.parseDouble(args[1]);
+        celsius = (fahrenheit - 32) * 5/9;
+        System.out.println("celsius is: " + celsius);
+      }
+      else if(args[0].equals("-C")){ // init value is celcius
+        celsius = Double.parseDouble(args[1]);
+        fahrenheit = (celsius * 9/5) + 32;
+        System.out.println("farenheit is: " + fahrenheit);
+      }
+      else{ // first arg is wrong
+        // print error
+        System.out.println("illegal first argument");
+      }
     }
-    else if(args[0].equals("-C")){ // init value is celcius
-      celsius = Double.parseDouble(args[1]);
-      fahrenheit = (celsius * 9/5) + 32;
-      System.out.println("farenheit is: " + fahrenheit);
+    else{
+      System.out.println("illegal second argument");
     }
-    else{ // first arg is wrong
-      // print error
-      System.out.println("you did not put in the correct first argument");
-    }
-
-
-
-
-
-
 
   }
-
 }
